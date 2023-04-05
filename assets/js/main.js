474229a9d7483577c98e5332a203df4f448a5f26
angelgit <angelgugliuzzo@gmail.com>
2023-04-05 20:53:19 -0300

    probando post
    probando subir a git

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+async function saveAlbum(){
+
+    // capturo el valor de los dos input.
+    const userId = document.getElementById("userId").value;
+    const title = document.getElementById("title").value;
+
+    // verifico que no esten vacios
+    if(userId == 0 || title == ""){
+        return alert("Los campos solcitados no pueden ser nulos")
+    }
+    
+    // POST 
+    // https://jsonplaceholder.typicode.com/albums
+    // {
+    //     "userId" : 3,
+    //     "title": "Probando 4"
+    // }
+   
+    // crearmos y convertimos a json nuestro obj
+
+   const data = {"userId":userId, "title":title}
+   const dataJson = JSON.stringify(data)
+
+   // armamos nuestro request 
+   const response = await fetch('https://jsonplaceholder.typicode.com/albums',{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body:dataJson
+   })
+
+   const responseData = await response.json()
+
+   document.getElementById("respuesta").innerHTML = `
+   <h2>Se creo correctamente el album</h2> 
+  <p>id: ${responseData.id} y su titulo es: ${responseData.title}</p>`
+
+  document.getElementById("formularioAlbum").reset(); 
+
+}
